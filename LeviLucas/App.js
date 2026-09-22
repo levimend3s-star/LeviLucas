19933fdc150bca37fbd6f6d31023bb77a2a1d8fb
levimend3s-star <levimend3s@gmail.com>
2026-09-22 18:29:43 +0000

    v3

--- LeviLucas/App.js
+++ LeviLucas/App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "react-navigation/native-stack"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Login from './screens/Login'
+import Cadastro from './screens/Cadastro'
+import Home from './screens/Home'
+
+const Stack=createNativeStackNavigator()
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen>
+          name='Login'
+          component={Login}
+        </Stack.Screen>
+        <Stack.Screen>
+          name='Cadastro'
+          component={Cadastro}
+        </Stack.Screen>
+        <Stack.Screen>
+          name='Home'
+          component={Home}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
